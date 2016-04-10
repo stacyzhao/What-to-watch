00bed910c85a4b5e89e6ca6db4d48dff514315c4
Stacy Zhao <stacyzhao@hotmail.com>
2016-04-10 09:26:47 -0400

    added more functions

--- movie_lib.py
+++ movie_lib.py
@@ -28,7 +28,6 @@ with open('u.item.csv', encoding='latin_1') as f:
 #     movie = Movie(row["movie_id"], row["title"])
 #     movies[movie.id] = movie
 #
-# movies_list = [movies[key] for key in movies]
 # print (str(movies["95"]))
 
 movie_ratings = []
@@ -41,19 +40,22 @@ with open("u.data.csv", encoding='latin_1') as f:
         # user_ratings[row["user_id"]] = rating
         movie_ratings.append(rating)
 
+for rating in movie_ratings:
+    movie = movies[rating.item_id]
+    movie.add_rating(rating)
+
 movies_list = [movies[key] for key in movies]
 
 
 # Find all ratings for a movie by id - returns a list of all ratings by movie id
 
-def all_ratings_by_movie_id(ratings, movie_id):
-    all_ratings = {}
+def all_ratings_by_movie_id(ratings):
     for rating in ratings:
-        if rating.item_id == movie_id:
-            all_ratings[movie.id].append(rating)
-    return all_ratings
-# print (all_ratings_by_movie_id(movie_ratings, "757"))
-sevenfiveseven = all_ratings_by_movie_id(movie_ratings, "863")
+        movie = movies[rating.item_id]
+        movie.add_rating(rating)
+
+# print (all_ratings_by_movie_id(movie_ratings, movies[movie.id]))
+# print(all_ratings_by_movie_id(movie_ratings, movie.id))
 
 # Find all ratings for a user - returns a list of ratings by user id
 
@@ -68,13 +70,13 @@ def all_ratings_by_user(ratings, user_id):
 
 # Find the average rating for a movie by id - an average rating of a movie by id
 
-def average_rating(ratings):
-    sum_of_rating = 0
-# ratings_per_movie = all_ratings_by_movie_id(movie_ratings, "333")
-    for rating in ratings:
-        sum_of_rating += rating.rating
-    average_rating = sum_of_rating/len(ratings)
-    return average_rating
+# def average_rating(ratings):
+#     sum_of_rating = 0
+# # ratings_per_movie = all_ratings_by_movie_id(movie_ratings, "333")
+#     for rating in ratings:
+#         sum_of_rating += rating.rating
+#     average_rating = sum_of_rating/len(ratings)
+#     return average_rating
 
 # print (average_rating(sevenfiveseven))
 
@@ -85,24 +87,58 @@ def name_of_movie_by_id(movie_id):
 
 # print (name_of_movie_by_id(333))
 
+def filter_movies(movies):
+    filtered_movies = {}
+    for movie_id, movie in movies.items():
+        if len(movie.ratings) > 200:
+            filtered_movies[movie_id] = movie
+    return filtered_movies
+# print (filter_movies(movies))
 
+# def avg_rating_by_movie(movies):
+#     movie_avg_rating = {}
+#     for movie_id, movie in movies.items():
+#         ratings = all_ratings_by_movie_id(movie_ratings, movie_id)
+#         average = average_rating(ratings)
+#         movie_avg_rating[movie_id] = round(average,2)
+#     return movie_avg_rating
+# print (avg_rating_by_movie(filter_movies))
 
 def avg_rating_by_movie(movies):
+    filtered_movies = filter_movies(movies)
     movie_avg_rating = {}
     for movie_id, movie in movies.items():
-        ratings = all_ratings_by_movie_id(movie_ratings, movie_id)
-        average = average_rating(ratings)
-        movie_avg_rating[movie_id] = round(average,2)
+        ratings = round(movie.average_rating(),2)
+        if movie_id in filtered_movies:
+            movie_avg_rating[movie_id] = ratings
     return movie_avg_rating
-
 # print (avg_rating_by_movie(movies))
-def get_min_rating(movies):
-    min_rating_movie = {}
-    for movie_id, movie in movies.items():
-        ratings = all_ratings_by_movie_id(movie_ratings, movie_id)
-        for rating in ratings:
-            if len(ratings) > 20:
-                min_rating_movie[movie_id].append(ratings)
-    return min_rating_movie
-#
-print(get_min_rating(movies))
+
+def top_movies_list(avg_rating, movies):
+    top_movie_list = {}
+    for movie_id, avg in avg_rating.items():
+        if avg > 4.0:
+         top_movie_list[movie_id] = movies[movie_id].title
+    return top_movie_list
+
+
+
+# print (top_movies_list(avg_rating_by_movie(movies),movies))
+# print name_of_movie_by_id()
+
+
+
+
+
+
+
+# def get_min_rating(movies):
+#     min_rating_movie = {}
+#     for movie_id, movie in movies.items():
+#         print(movie_id, movie)
+#         ratings = all_ratings_by_movie_id(movie_ratings, movie_id)
+#         for rating in ratings:
+#             if len(ratings) > 20:
+#                 min_rating_movie[movie_id].append(rating.rating)
+#     return min_rating_movie
+# print(get_min_rating(movie_ratings))
