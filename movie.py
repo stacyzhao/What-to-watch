53bb00b310127a7063bae9bb57878b80ed3bd3c8
Stacy Zhao <stacyzhao@hotmail.com>
2016-04-10 09:25:48 -0400

    added methods - get avg and rating to movie.py

--- movie.py
+++ movie.py
@@ -2,6 +2,18 @@ class Movie():
     def __init__(self, movie_id, title):
         self.id = movie_id
         self.title = title
+        self.ratings = []
 
     def __str__(self):
         return "{} : {}", self.id, self.title
+
+
+    def add_rating(self, rating):
+        self.ratings.append(rating)
+
+    def average_rating(self):
+        sum_of_rating = 0
+        for rating in self.ratings:
+            sum_of_rating += rating.score
+        average_rating = sum_of_rating/len(self.ratings)
+        return average_rating
